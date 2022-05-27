9f5846c006f1a7a4ffe646a36fe5b05ae1bd9bcc
Camilo <dlt.camilo@gmail.com>
2022-05-27 15:36:33 +0200

    send in loop

--- Capteurs/Capteur2/scripts/send_new.py
+++ Capteurs/Capteur2/scripts/send_new.py
@@ -28,46 +28,42 @@ data = e.give_measure_info()
 meta = e.give_meta_info()
 assert data is not None
 
-# INJECTION ------
-logger.info("Creating Injection instance")
-inject = Injection(dbname=DB_NAME_, meta=meta, df=data, ip=CON_)
+# # INJECTION ------
+# logger.info("Creating Injection instance")
+# inject = Injection(dbname=DB_NAME_, meta=meta, df=data, ip=CON_)
 
-logger.info("Injecting data")
-inject.injection()
-logger.info("Injected")
+# logger.info("Injecting data")
+# inject.injection()
+# logger.info("Injected")
 
 # RUN MAIN  ------
-# # LOOP
-# today = datetime.datetime.today()
-# day_init = today - datetime.timedelta(days=80)  # old 140  old 200
-# days = 20
-# for i in range(days):
-#     from_ = day_init.strftime("%Y-%m-%d")
-#     to_ = day_init + datetime.timedelta(days=1)
-#     to_ = to_.strftime("%Y-%m-%d")
-#     print(f"Doing {from_} to : {to_}")
-#     try:
-#         enedis_instance = ENEDIS(
-#             start_date=from_,
-#             end_date=to_,
-#             config_file="new_compteur/config.json",
-#         )
-#         data = enedis_instance.give_data()
-#         inject = Injection(
-#             dbname="enedis_camilo", meta=("1", "enedis"), df=data, ip="localhost"
-#         )
-#         inject.injection()
-#         logger.info("Injected")
-#     except Exception as e:
-#         logger.error("Exception catched")
-#         print(e)
+# LOOP
+import datetime
+import time
 
-#     day_init = day_init + datetime.timedelta(days=1)
-#     time.sleep(30)
+today = datetime.datetime.today()
+day_init = today - datetime.timedelta(days=50)  # old 140  old 200
+days = 20
+for i in range(days):
+    from_ = day_init.strftime("%Y-%m-%d")
+    to_ = day_init + datetime.timedelta(days=1)
+    to_ = to_.strftime("%Y-%m-%d")
+    print(f"Doing {from_} to : {to_}")
+    try:
+        e = ENEDIS(
+            start_date=from_,
+            end_date=to_,
+            config_file=CONFIG_,
+        )
+        # * Get data ---
+        data = e.give_measure_info()
+        meta = e.give_meta_info()
+        inject = Injection(dbname=DB_NAME_, meta=meta, df=data, ip=CON_)
+        inject.injection()
+        logger.info("Injected")
+    except Exception as e:
+        logger.error("Exception catched")
+        print(e)
 
-
-# inject = Injection(dbname="enedis_final", meta=("1", "enedis"), df=data, ip="localhost")
-# inject.injection()
-
-
-# enedis_data.to_csv("data.csv")
+    day_init = day_init + datetime.timedelta(days=1)
+    time.sleep(30)
